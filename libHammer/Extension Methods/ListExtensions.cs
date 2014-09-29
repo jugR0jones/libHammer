f0d0f70c3d0b8ab312c89ea004a5654d9714a286
jugR0Jones <mr.g.mcintosh@gmail.com>
2014-09-29 13:03:24 +0200

    Added extensions to the UrlHelper and HttpSessionStateBase classes.

---
+++ libHammer/Extension Methods/ListExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace libHammer
+{
+    
+    /// <summary>
+    /// Extensions to the C# List
+    /// </summary>
+    public static class ListExtensions<T>
+    {
+
+        /// <summary>
+        /// Insert an item at the top of the list.
+        /// NOTE: This creates a new list.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        //public static List<T> InsertFirst<T>(this List<T> list, T item)
+        //{
+        //    List<T> newList = new List<T>();
+
+        //    newList.Add(item);
+        //    newList.AddRange(list);
+
+        //    return newList;
+        //}
+        
+    }
+}
