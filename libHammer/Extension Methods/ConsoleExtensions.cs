8ea3d673e39d3af85c5e75982f14977cd38ba2e5
jugR0jones <mr.g.mcintosh@gmail.com>
2014-09-30 19:07:21 +0200

    Added console extension methods

---
+++ libHammer/Extension Methods/ConsoleExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libHammer.Extension_Methods
+{
+
+    /// <summary>
+    /// Extensions to the Console class.
+    /// </summary>
+    public static class ConsoleExtensions
+    {
+
+        public const ConsoleColor ErrorColor = ConsoleColor.Red;
+        public const ConsoleColor WarningColor = ConsoleColor.Yellow;
+        public const ConsoleColor SuccessColor = ConsoleColor.Green;
+        public const ConsoleColor InfoColor = ConsoleColor.Gray;
+
+        public static WriteErrorLine(string msg, params object[] args) {
+            this.WriteLine(ErrorColor, msg, args);
+        }
+
+    }
+}
