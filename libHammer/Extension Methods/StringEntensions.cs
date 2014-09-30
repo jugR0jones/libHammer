aa9b465aee4509e6ebe491ca6713de12994361dd
jugR0jones <mr.g.mcintosh@gmail.com>
2014-09-30 21:41:04 +0200

    Fixing problems with commits

---
+++ libHammer/Extension Methods/StringEntensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libHammer.Extension_Methods
+{
+
+    /// <summary>
+    /// Extension methods to the String class.
+    /// </summary>
+    public static class StringEntensions
+    {
+
+        /// <summary>
+        /// Converts a string to its enum value.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static T Parse<T>(this string value)
+        {
+            return (T)Enum.Parse(typeof(T), value, true);
+        }
+
+    }
+}
---
+++ libHammer/Helper Methods/ConsoleHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libHammer.Helper_Methods
+{
+
+    /// <summary>
+    /// Methods that extend the usefullness of the console.
+    /// </summary>
+    public class ConsoleHelper
+    {
+    }
+}
