216a6d44ca77e01756baf2772e9899e0aa7179ed
jugR0jones <mr.g.mcintosh@gmail.com>
2014-09-30 22:13:22 +0200

    Fixed bugs with ConsoleHelper class.

--- libHammer/Helper Methods/ConsoleHelper.cs
+++ libHammer/Helper Methods/ConsoleHelper.cs
@@ -28,46 +28,78 @@ namespace libHammer.Helper_Methods
         /// <param name="args"></param>
         public static void ErrorLine(string message, params object[] args)
         {
-            ConsoleExtensions.WriteLine(ConsoleExtensions.ErrorColor, message, args);
+            ConsoleHelper.WriteLine(ConsoleHelper.ErrorColor, message, args);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="args"></param>
         public static void Error(string message, params object[] args)
         {
-            ConsoleExtensions.Write(ConsoleExtensions.ErrorColor, message, args);
+            ConsoleHelper.Write(ConsoleHelper.ErrorColor, message, args);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="args"></param>
         public static void WarnLine(string msg, params object[] args)
         {
-            WriteLine(WarningColor, msg, args);
+            ConsoleHelper.WriteLine(WarningColor, msg, args);
         }
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="args"></param>
         public static void Warn(string msg, params object[] args)
         {
-            Write(WarningColor, msg, args);
+            ConsoleHelper.Write(WarningColor, msg, args);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="args"></param>
         public static void InfoLine(string msg, params object[] args)
         {
-            WriteLine(InfoColor ?? Console.ForegroundColor, msg, args);
+            ConsoleHelper.WriteLine(ConsoleHelper.InfoColor, msg, args);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="args"></param>
         public static void Info(string msg, params object[] args)
         {
-            Write(InfoColor ?? Console.ForegroundColor, msg, args);
+            ConsoleHelper.Write(ConsoleHelper.InfoColor, msg, args);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="args"></param>
         public static void SuccessLine(string msg, params object[] args)
         {
             WriteLine(SuccessColor, msg, args);
         }
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="args"></param>
         public static void Success(string msg, params object[] args)
         {
             Write(SuccessColor, msg, args);
         }
-
 
         /// <summary>
         /// Clears the current line.
@@ -83,25 +115,47 @@ namespace libHammer.Helper_Methods
             Console.SetCursorPosition(0, Console.CursorTop);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="args"></param>
         public static void Write(string msg, params object[] args)
         {
-            Console.Write(msg, args);
+            ConsoleHelper.Write(ConsoleHelper.InfoColor, msg, args);
         }
 
-
-        public static void WriteLine(ConsoleColor color, string msg, params object[] args)
-        {
-            Write(color, msg, args);
-            Console.Out.WriteLine();
-        }
-
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="color"></param>
+        /// <param name="msg"></param>
+        /// <param name="args"></param>
         public static void Write(ConsoleColor color, string msg, params object[] args)
         {
             try
             {
                 Console.ForegroundColor = color;
                 Console.Write(msg, args);
+            }
+            finally
+            {
+                Console.ResetColor();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="color"></param>
+        /// <param name="message"></param>
+        /// <param name="args"></param>
+        public static void WriteLine(ConsoleColor color, string message, params object[] args)
+        {
+            try
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine(message, args);
             }
             finally
             {
